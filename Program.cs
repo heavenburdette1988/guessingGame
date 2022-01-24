d254f091eb983017a8ca7b1df4c061fbd29d4af6
heavenburdette1988 <heavenburdette1988@gmail.com>
2022-01-24 14:01:12 -0500

    6 complete

--- Program.cs
+++ Program.cs
@@ -8,25 +8,29 @@ namespace guessingGame
         {
 
              
-            Console.Write("Can you guess the secret number? ");
- int secretNum = new Random().Next(1, 99);
+            Console.Write("Can you guess the secret number between 1 - 100? ");
 
+ int secretNum = new Random().Next(1, 100);
+  
 
         for (int i = 0; i < 4; i++){
 
-            int num = Convert.ToInt32(Console.ReadLine());
+           int num = Convert.ToInt32(Console.ReadLine());
                 
-           
-                
-           
             if(num == secretNum){
-                Console.Write($"You guessed the number correct!!!{secretNum}");
+                Console.Write($"You guessed the number correct, {secretNum}!!!");
                 break;
 
                            } else {
-            Console.WriteLine(" Wrong! try again" + $"Your Guess {-i +4}!" + $"{secretNum}");
-            
-            } 
+
+            Console.WriteLine(" Wrong! try again");
+            Console.WriteLine($" {-i +4} guesses remaining!" + $"{secretNum}");
+                        } 
+             if(secretNum > num ){
+                 Console.WriteLine("Your guess is to low");
+             }else  if(secretNum < num ){
+                   Console.WriteLine("Your guess is to high");
+             }
             }        
         } 
     }
