ce9b806f01bc62e0da52533289bf7b9574fb3202
heavenburdette1988 <heavenburdette1988@gmail.com>
2022-01-24 15:22:42 -0500

    7 complete

--- Program.cs
+++ Program.cs
@@ -7,31 +7,129 @@ namespace guessingGame
         public static void Main(string[] args)
         {
 
-             
-            Console.Write("Can you guess the secret number between 1 - 100? ");
+ Console.Write("What difficulty Level do you want, easy, medium, hard? ");
+string easy = "easy";
+string medium = "medium";
+string hard = "hard";
+string Answer = Console.ReadLine();
 
- int secretNum = new Random().Next(1, 100);
+ if(easy == Answer){
+Console.WriteLine("Can you guess the secret number between 1 - 100? You get 8 guesses");
+
+int secretNum = new Random().Next(1, 100);
   
-
-        for (int i = 0; i < 4; i++){
+        for (int i = 0; i < 8; i++){
 
            int num = Convert.ToInt32(Console.ReadLine());
+           if(num == secretNum){
                 
-            if(num == secretNum){
                 Console.Write($"You guessed the number correct, {secretNum}!!!");
                 break;
 
-                           } else {
+           }   else {
 
             Console.WriteLine(" Wrong! try again");
-            Console.WriteLine($" {-i +4} guesses remaining!" + $"{secretNum}");
-                        } 
-             if(secretNum > num ){
+            Console.WriteLine($" {-i +7} guesses remaining!" + $"{secretNum}");
+//                         
+        } if(secretNum > num ){
+
                  Console.WriteLine("Your guess is to low");
-             }else  if(secretNum < num ){
+
+            } else  if (secretNum < num ){
                    Console.WriteLine("Your guess is to high");
              }
-            }        
-        } 
+ }
+ }
+ 
+ 
+ else if(medium == Answer){
+Console.WriteLine("Can you guess the secret number between 1 - 100? You get 6 guesses");
+
+int secretNum = new Random().Next(1, 100);
+  
+        for (int i = 0; i < 6; i++){
+
+           int num = Convert.ToInt32(Console.ReadLine());
+           if(num == secretNum){
+                
+                Console.Write($"You guessed the number correct, {secretNum}!!!");
+                break;
+
+           }   else {
+
+            Console.WriteLine(" Wrong! try again");
+            Console.WriteLine($" {-i +5} guesses remaining!" + $"{secretNum}");
+//                         
+        } if(secretNum > num ){
+
+                 Console.WriteLine("Your guess is to low");
+
+            } else  if (secretNum < num ){
+                   Console.WriteLine("Your guess is to high");
+             }
+ }
+} 
+
+ else if(hard == Answer) {
+    Console.WriteLine("Can you guess the secret number between 1 - 100? You get 4 guesses");
+    int secretNum = new Random().Next(1, 100);
+  
+        for (int i = 0; i < 4; i++){
+
+           int num = Convert.ToInt32(Console.ReadLine());
+           if(num == secretNum){
+                
+                Console.Write($"You guessed the number correct, {secretNum}!!!");
+                break;
+
+           }   else {
+
+            Console.WriteLine(" Wrong! try again");
+            Console.WriteLine($" {-i +3} guesses remaining!" + $"{secretNum}");
+//                         
+        } if(secretNum > num ){
+
+                 Console.WriteLine("Your guess is to low");
+
+            } else  if (secretNum < num ){
+                   Console.WriteLine("Your guess is to high");
+             }
+ }
+}
+        }
     }
 }
+
+
+       
+   
+ 
+ 
+ 
+
+
+//  Console.Write("Can you guess the secret number between 1 - 100? ");
+
+//  
+                
+//             if(num == secretNum){
+//                 Console.Write($"You guessed the number correct, {secretNum}!!!");
+//                 break;
+
+//                            } else {
+
+//             Console.WriteLine(" Wrong! try again");
+//             Console.WriteLine($" {-i +4} guesses remaining!" + $"{secretNum}");
+//                         } 
+//              if(secretNum > num ){
+
+//                  Console.WriteLine("Your guess is to low");
+
+//             } else  if (secretNum < num ){
+//                    Console.WriteLine("Your guess is to high");
+//              }
+//             }        
+//         } }
+    
+        
+//  }}
