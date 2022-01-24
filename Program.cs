8c2a395982848521152105a842e076b64d230835
heavenburdette1988 <heavenburdette1988@gmail.com>
2022-01-24 11:04:11 -0500

    phase 2

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace guessingGame
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.Write("Can you guess the secret number?");
+            int num = Convert.ToInt32(Console.ReadLine());
+            int secretNum = 42;
+            if(num == secretNum)
+            {
+                Console.Write("You guessed the number");
+            } else{
+                 Console.Write("Wrong!");
+            }
+        }
+    }
+}
